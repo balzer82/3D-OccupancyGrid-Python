033d1059abc5e7029f4f5a49a8907ac3cc375b94
Paul Balzer <balzer82@gmail.com>
2016-08-22 10:06:33 +0200

    Blensor

--- 3D-Occupancy-Grid-ibeo-Lux-BlenSor.py
+++ 3D-Occupancy-Grid-ibeo-Lux-BlenSor.py
@@ -3,11 +3,13 @@
 
 # # 3D Occupancy Grid with ibeo Lux Laserscanner Data from BlenSor
 # 
-# [BlenSor](http://blensor.org) is a Laserscanner simulation Toolbox, based on Blender
+# [BlenSor](http://blensor.org) is a Laserscanner simulation Toolbox, based on Blender, Thanks Michael!
+# 
+# ```
+# Michael Gschwandtner, Roland Kwitt, Andreas Uhl and Wolfgang Pree,  BlenSor: Blender Sensor Simulation Toolbox , In G. Bebis, R. Boyle, B. Parvin, D. Koracin, R. Chung and R. Hammoud, editors, Advances in Visual Com- puting: 7th International Symposium, (ISVC 2011), Volume 6939/2011, pp. 199-208, Springer Verlag, 2011
+# ```
 
-# ![ibeo Lux](http://www.mechlab.de/wp-content/uploads/2012/02/ibeoLUX.jpg)
-
-# In[16]:
+# In[202]:
 
 import numpy as np
 import time
@@ -15,7 +17,7 @@ import pandas as pd
 import pickle
 
 
-# In[17]:
+# In[203]:
 
 get_ipython().magic(u'matplotlib inline')
 import matplotlib.pyplot as plt
@@ -25,9 +27,6 @@ from matplotlib import cm
 import seaborn as sns
 sns.set_style('whitegrid')
 
-from IPython.html.widgets import interact
-from IPython.html import widgets
-
 
 # In[ ]:
 
@@ -36,18 +35,18 @@ from IPython.html import widgets
 
 # ### Create Empty Grid
 
-# In[18]:
+# In[204]:
 
-l = 10.0 # Länge m
-b = 10.0  # Breite m
-h = 2.0  # Höhe m
+l = 50.0 # Länge m
+b = 35.0  # Breite m
+h = 3.0  # Höhe m
 
-r = 0.1 # Resolution m/gridcell
+grid_res = 0.1 # Resolution m/gridcell
 
 
-# In[19]:
+# In[205]:
 
-print('%.1fmio Grid Cells' % ((l*b*h)/r**3/1e6))
+print('%.1fmio Grid Cells' % ((l*b*h)/grid_res**3/1e6))
 
 
 # ## Generate a LogOdds Grid
@@ -59,19 +58,19 @@ print('%.1fmio Grid Cells' % ((l*b*h)/r**3/1e6))
 # 
 # So let's use it!
 
-# In[20]:
+# In[206]:
 
-print "%ix%ix%i Grid" % (l/r, b/r, h/r)
+print "%ix%ix%i Grid" % (l/grid_res, b/grid_res, h/grid_res)
 startTime = time.time()
 
-grid = np.zeros((l/r, b/r, h/r), dtype=np.float32) # Log Odds Grid must be initialized with zeros!
+grid = np.zeros((l/grid_res, b/grid_res, h/grid_res), dtype=np.float32) # Log Odds Grid must be initialized with zeros!
 
 print "Stats: %.2fs, %.2fGB" % (time.time() - startTime, (grid.nbytes/1024.0**2))
 
 
-# ### 3D View
+# ### Function for 3D View of the Occupancy Grid
 
-# In[21]:
+# In[207]:
 
 def plot3Dgrid(grid, az, el):
     # plot the surface
@@ -98,18 +97,39 @@ def plot3Dgrid(grid, az, el):
     return plt3d
 
 
-# In[22]:
+# ## 3D Rotation Matrix in `yaw, pitch, roll` convention
+# 
+# see "[[Tutorial] Rotationsmatrix und Quaternion einfach erklärt in DIN70000 ZYX Konvention](http://www.cbcity.de/tutorial-rotationsmatrix-und-quaternion-einfach-erklaert-in-din70000-zyx-konvention)" for more details
 
-#plot3Dgrid(grid, 25, -30)
+# In[208]:
+
+def Rypr(y, p, r, in_deg=False):
+    '''
+    Rotationsmatrix für y=yaw, p=pitch, r=roll in degrees
+    '''
+    if in_deg:
+        # from Degree to Radians
+        y = y*np.pi/180.0
+        p = p*np.pi/180.0
+        r = r*np.pi/180.0
+
+    Rr = np.matrix([[1.0, 0.0, 0.0],[0.0, np.cos(r), -np.sin(r)],[0.0, np.sin(r), np.cos(r)]])
+    Rp = np.matrix([[np.cos(p), 0.0, np.sin(p)],[0.0, 1.0, 0.0],[-np.sin(p), 0.0, np.cos(p)]])
+    Ry = np.matrix([[np.cos(y), -np.sin(y), 0.0],[np.sin(y), np.cos(y), 0.0],[0.0, 0.0, 1.0]])
+    
+    return Ry*Rp*Rr
 
 
 # ## Integrate a measurement with BRESENHAM Algorithm
 
+# Based on:
+# ```
 # Amanatides, J., & Woo, A. (1987). A fast voxel traversal algorithm for ray tracing. Proceedings of EUROGRAPHICS, i. Retrieved from http://www.cse.yorku.ca/~amana/research/grid.pdf
+# ```
 # 
 # Here is a Python Implementation of BRESENHAM Algorithm: https://gist.github.com/salmonmoose/2760072
 
-# In[23]:
+# In[209]:
 
 def bresenham3D(startPoint, endPoint):
     # by Anton Fletcher
@@ -170,109 +190,14 @@ def bresenham3D(startPoint, endPoint):
     return path
 
 
-# In[24]:
+# ## Function which integrates the Measurements via Inverse Sensor Model
 
-def Rypr(y, p, r, in_deg=False):
-    '''
-    Rotationsmatrix für y=yaw, p=pitch, r=roll in degrees
-    '''
-    if in_deg:
-        # from Degree to Radians
-        y = y*np.pi/180.0
-        p = p*np.pi/180.0
-        r = r*np.pi/180.0
-
-    Rr = np.matrix([[1.0, 0.0, 0.0],[0.0, np.cos(r), -np.sin(r)],[0.0, np.sin(r), np.cos(r)]])
-    Rp = np.matrix([[np.cos(p), 0.0, np.sin(p)],[0.0, 1.0, 0.0],[-np.sin(p), 0.0, np.cos(p)]])
-    Ry = np.matrix([[np.cos(y), -np.sin(y), 0.0],[np.sin(y), np.cos(y), 0.0],[0.0, 0.0, 1.0]])
-    
-    return Ry*Rp*Rr
-
-
-# ### Load some Simulated Laserscanner Data
+# Values for hit and miss probabilities are taken from
 # 
-# The data was generated with [Blensor](http://blensor.org)
-
-# In[25]:
-
-motiondata = pd.read_csv('./scans/motion.pcd', delimiter=' ',
-                         usecols=[1,5,7,9,11,13,15],
-                         names=['timestamp','x','y','z','rot_x','rot_y','rot_z'],
-                         index_col='timestamp')
-motiondata.head()
-
-
-# Every `timestamp` with arbitrary `x, y, z` location and `rot_x, rot_y, rot_z` Rotation of the sensor has a scan:
-
-# In[ ]:
-
-
-
-
-# In[26]:
-
-len(motiondata)
-
-
-# Rotation und Translation in homogenen Koordinaten, d.h. es kann alles über Matrizenmultiplikation gemacht werden.
+#     Hornung, A., Wurm, K. M., Bennewitz, M., Stachniss, C., & Burgard, W. (2013). OctoMap: an efficient probabilistic 3D mapping framework based on octrees. Autonomous Robots, 34(3), 189–206. doi:10.1007/s10514-012-9321-0
 # 
-# $$\left[\begin{matrix}x \\ y \\ z \\ 1\end{matrix}\right]_\text{Global} = \left[\begin{matrix} R_{3x3} & t_{1x3} \\ 0 & 1\end{matrix}\right] \cdot \left[\begin{matrix}x \\ y \\ z \\ 1\end{matrix}\right]_\text{Lokal}$$
-# 
-# wobei $R$ die Rotationsmatrix ist und $t$ der Verschiebungsvektor
 
-# In[54]:
-
-m_start = 320 # Startnummer
-pointcloud = pd.DataFrame()
-
-T = np.eye(4)
-R = Rypr(0.0, 0.0, 0.0, in_deg=True)
-T[:3,:3] = R
-
-for m in range(len(motiondata)):
-    # some ibeo lux simulated measurements
-    data = pd.read_csv('./scans/scan_%i.pcd' % (m+m_start), delimiter=' ',
-                       skiprows=11, names=['X','Y','Z','rgb','label'])
-    #data.head(5)
-    
-    # Filter with euclidean Distance from scanner
-    max_dist = 10.0
-    min_dist = 1.5
-    data['dist'] = np.sqrt(data.X**2.0 + data.Y**2.0 + data.Z**2.0)
-    data = data[(data['dist']<max_dist) & (data['dist']>min_dist)]
-
-    # Transformation von Lokal in Global
-    t = np.array([0, 0, 0])
-    T[:3, 3] = t
-    
-    pcl_local_homogen = np.vstack([data[['X','Y','Z']].T, np.ones([1, len(data)])])
-    pcl_global_homogen = np.dot(T, pcl_local_homogen)
-    data[['x','y','z']] = pd.DataFrame(pcl_global_homogen[:3, :].T)
-                                       
-    pointcloud = pointcloud.append(data[['x','y','z']])
-
-
-# In[52]:
-
-pointcloud.plot(x='x', y='y', c='z', kind='scatter', alpha=0.5, cmap='summer')
-
-
-# In[53]:
-
-plt3d = plt.figure(figsize=(12, 6)).gca(projection='3d', axisbg='w')
-plt3d.scatter(pointcloud.x[::2], pointcloud.y[::2], pointcloud.z[::2], c='r', label='Laserscanner Pointcloud')
-#plt3d.scatter(t[0], t[1], t[2], c='k', s=200, label='ibeo Lux')
-plt3d.view_init(15, 145)
-plt3d.axis('equal')
-plt3d.set_xlabel('X')
-plt3d.set_ylabel('Y')
-
-
-# ## Function which integrates the Measurement via Inverse Sensor Model
-
-# Values for hit and miss probabilities are taken from Hornung, A., Wurm, K. M., Bennewitz, M., Stachniss, C., & Burgard, W. (2013). OctoMap: an efficient probabilistic 3D mapping framework based on octrees. Autonomous Robots, 34(3), 189–206. doi:10.1007/s10514-012-9321-0
-
-# In[134]:
+# In[210]:
 
 # in LogOdds Notation!
 loccupied = 0.85
@@ -282,9 +207,22 @@ lmin = -2.0
 lmax = 3.5
 
 
-# In[135]:
+# In[211]:
 
-def insertPointcloudBRESENHAM(tSensor, xe,ye,ze):
+def insertPointcloudBRESENHAM(grid, grid_res, tSensor, XYZendpoints):
+    '''
+    grid: Occupancy Grid
+    grid_res: Occupancy Grid Resolution
+    tSensor: x,y,z coordinate of sensor origin
+    XYZendpoints: x,y,z coordinate array of scan endpoints
+    '''
+    xe = XYZendpoints[:,0]/grid_res
+    ye = XYZendpoints[:,1]/grid_res
+    ze = XYZendpoints[:,2]/grid_res
+    
+    xs = tSensor[0]/grid_res
+    ys = tSensor[1]/grid_res
+    zs = tSensor[2]/grid_res    
     
     for i,val in enumerate(xe):
         
@@ -296,40 +234,127 @@ def insertPointcloudBRESENHAM(tSensor, xe,ye,ze):
         # Inverse Sensor Model
         grid[x,y,z] += loccupied # increase LogOdds Ratio
 
-        if grid[x,y,z]>lmax: #clamping
+        if grid[x,y,z]>lmax: # clamping
             grid[x,y,z]=lmax
 
         
         # Grid cells in perceptual range of laserscanner
-        for (y,x,z) in bresenham3D(tSensor, (xe[i], ye[i], ze[i])): # !!! Koordinatenswitch zwischen X & Y
+        for (y,x,z) in bresenham3D((xs,ys,zs), (xe[i], ye[i], ze[i])): # !!! Koordinatenswitch zwischen X & Y
 
             grid[x,y,z] += lfree # decrease LogOdds Ratio
 
-            if grid[x,y,z]<lmin: #clamping
-                grid[x,y,z]=lmin        
+            if grid[x,y,z]<lmin: # clamping
+                grid[x,y,z]=lmin
+
+    return grid
 
 
 # ### Sensor Origin
+# 
+# The grid is 0-based, the scans are in global coordinates, so we shift the scans to some coordinates, that it will fit into the initialized Occupancy grid.
 
-# In[136]:
+# In[212]:
 
-tSensor = t/r  # Translation (shift from 0,0,0) in Grid Cell Numbers
-tSensor
+grid_shift = np.array([40.0, 8.0, 0.0])
 
 
-# In[137]:
+# ### Load some Simulated Laserscanner Data
+# 
+# The data was generated with [Blensor](http://blensor.org):
+# 
+# 1. Some `.pcd` were dumped
+# 2. A `motion.pcd` was generated, which holds the position and orientation of the Laserscanner
 
-# integrate the measurement 5 times
-for m in range(5):
-    try:
-        insertPointcloudBRESENHAM(tSensor, xe/r,ye/r,ze/r)
-    except:
-        print('Fehler beim Einfügen der Messung. Grid zu klein gewählt?!')
+# In[213]:
+
+motiondata = pd.read_csv('./scans/motion.pcd', delimiter=' ',
+                         usecols=[1,5,7,9,11,13,15],
+                         names=['timestamp','x','y','z','rot_x','rot_y','rot_z'],
+                         index_col='timestamp')
+motiondata.head()
+
+
+# Every `timestamp` with arbitrary `x, y, z` location and `rot_x, rot_y, rot_z` Rotation of the sensor has a scan:
+
+# In[214]:
+
+len(motiondata)
+
+
+# Rotation und Translation in homogenen Koordinaten, d.h. es kann alles über Matrizenmultiplikation gemacht werden.
+# 
+# $$\left[\begin{matrix}x \\ y \\ z \\ 1\end{matrix}\right]_\text{Global} = \underbrace{\left[\begin{matrix} R_{3x3} & t_{1x3} \\ 0 & 1\end{matrix}\right]}_{_T} \cdot \left[\begin{matrix}x \\ y \\ z \\ 1\end{matrix}\right]_\text{Lokal}$$
+# 
+# wobei $R$ die Rotationsmatrix ist und $t$ der Verschiebungsvektor
+
+# In[215]:
+
+m_start = 320 # Startnummer der Dateien (entspricht Frame in Blender)
+pointcloud = pd.DataFrame()
+
+T = np.eye(4)
+T[:3,:3] = Rypr(0.0, 0.0, 0.0, in_deg=True) # Rotationsmatrix
+
+for m in range(len(motiondata)):
+    # some ibeo lux simulated measurements
+    fname = './scans/scan_%i.pcd' % (m+m_start)
+    #print('Lade %s' % fname)
+    data = pd.read_csv(fname, delimiter=' ',
+                       skiprows=11, names=['X','Y','Z','rgb','label'])
+    #data.head(5)
+    
+    # Filter with euclidean Distance from scanner
+    max_dist = 15.0
+    min_dist = 1.5
+    data['dist'] = np.sqrt(data.X**2.0 + data.Y**2.0 + data.Z**2.0)
+    data = data[(data['dist']<max_dist) & (data['dist']>min_dist)]
+
+    # Transformation von Lokal in Global
+    t = np.array([motiondata.iloc[m].x, motiondata.iloc[m].y, motiondata.iloc[m].z])
+    T[:3, 3] = t+grid_shift
+    
+    # Homogene Koordinatentransformation (Rotation+Translation)
+    pcl_local_homogen = np.vstack([data[['X','Y','Z']].T, np.ones([1, len(data)])])
+    pcl_global = np.dot(T, pcl_local_homogen)[:3, :].T
+    
+    
+    grid = insertPointcloudBRESENHAM(grid, grid_res, t+grid_shift, pcl_global)
+    pointcloud = pointcloud.append(pd.DataFrame(pcl_global, columns=['x','y','z']), ignore_index=True)
+
+
+# In[ ]:
+
+
+
+
+# In[216]:
+
+pointcloud.plot(x='x', y='y', c='z', kind='scatter', cmap='summer', figsize=(14,5))
+plt.axis('equal')
+plt.title('Scans (view from top)')
+
+
+# In[217]:
+
+print('Maximale Pointcloud Abmessungen:')
+print('x: %.2f %.2f' % (pointcloud.x.min(), pointcloud.x.max()))
+print('y: %.2f %.2f' % (pointcloud.y.min(), pointcloud.y.max()))
+print('z: %.2f %.2f' % (pointcloud.z.min(), pointcloud.z.max()))
+
+
+# In[218]:
+
+plot3Dgrid(grid, 45, -115)
+
+
+# In[ ]:
+
+
 
 
 # ### 2D Plot of Grid Layer Z
 
-# In[27]:
+# In[ ]:
 
 @interact
 def plotmultivargauss(z = widgets.FloatSliderWidget(min=0, max=np.max(grid.shape[2])-1, step=1, value=10, description="")):
@@ -342,7 +367,7 @@ def plotmultivargauss(z = widgets.FloatSliderWidget(min=0, max=np.max(grid.shape
 
 # ### 3D Plot
 
-# In[28]:
+# In[ ]:
 
 @interact
 def plotmultivargauss(az = widgets.FloatSliderWidget(min=-90.0, max=90.0, step=1.0, value=45.0, description=""),                       el = widgets.FloatSliderWidget(min=-180.0, max=180.0, step=1.0, value=-115.0, description="")):
@@ -350,7 +375,7 @@ def plotmultivargauss(az = widgets.FloatSliderWidget(min=-90.0, max=90.0, step=1
     plot3Dgrid(grid, az, el)
 
 
-# In[29]:
+# In[97]:
 
 print('Max Grid Value (Log Odds): %.2f' % np.max(grid))
 print('Min Grid Value (Log Odds): %.2f' % np.min(grid))
@@ -358,7 +383,7 @@ print('Min Grid Value (Log Odds): %.2f' % np.min(grid))
 
 # #### Dump the Occupancy Grid to file
 
-# In[30]:
+# In[ ]:
 
 pklfile = open('occupancy-grid-LogOdds.pkl', 'wb')
 pickle.dump(grid, pklfile)
@@ -371,46 +396,46 @@ pklfile.close()
 # 
 # $$P(l) = 1-\cfrac{1}{1+e^{lo}}$$ with $lo$=LogOdds Value
 
-# In[31]:
+# In[ ]:
 
 gridP = np.asarray([1.0-(1.0/(1.0+np.exp(lo))) for lo in grid])
 
 
-# In[32]:
+# In[ ]:
 
 plot3Dgrid(gridP, 45, -115)
 plt.savefig('3D-Occupancy-Grid.png')
 
 
-# In[33]:
+# In[ ]:
 
 print('Max Grid Value (Probability): %.2f' % np.max(gridP))
 print('Min Grid Value (Probability): %.2f' % np.min(gridP))
 
 
-# In[34]:
+# In[ ]:
 
 print('Done.')
 
 
 # ## Convolve the Map for Path Planning
 
-# In[35]:
+# In[ ]:
 
 from scipy.ndimage import gaussian_filter
 
 
-# In[36]:
+# In[ ]:
 
 blurmap = gaussian_filter(gridP, 0.4)
 
 
-# In[37]:
+# In[ ]:
 
 plot3Dgrid(blurmap, 45, -115)
 
 
-# In[38]:
+# In[ ]:
 
 print('Max Grid Value (Probability): %.2f' % np.max(blurmap))
 print('Min Grid Value (Probability): %.2f' % np.min(blurmap))
@@ -418,7 +443,7 @@ print('Min Grid Value (Probability): %.2f' % np.min(blurmap))
 
 # #### Dump the convolved map
 
-# In[39]:
+# In[ ]:
 
 pklfile = open('occupancy-grid-Blur.pkl', 'wb')
 pickle.dump(blurmap, pklfile)
